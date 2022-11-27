da7f8c054b63732420295f15102f7e2a70437222
Michael Drummond <michael.drummond328@gmail.com>
2022-11-27 12:04:57 -0700

    mvp completed

--- main.py
+++ main.py
@@ -8,40 +8,56 @@ BACKGROUND_COLOR = "#B1DDC6"
 CARD_FLIP_TIME = 3 * 1000
 
 # globals
-wait_time = None
+current_card = {}
+
+# file
+try:
+    dataframe = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    words_data = original_data.to_dict(orient="records")
+else:
+    words_data = dataframe.to_dict(orient="records")
 
 
 # functions
 #   get new word
 def next_card():
+    global wait_time, current_card
+    window.after_cancel(wait_time)
+
     flash_card.itemconfig(flash_card_img, image=card_front_img)
 
-    random_word = random.choice(words_data)
-    french_word = random_word['French']
+    current_card = random.choice(words_data)
+    french_word = current_card['French']
 
     flash_card.itemconfig(display_word, text=french_word, fill="black")
     flash_card.itemconfig(display_title, text="French", fill="black")
 
-    english_word = random_word['English']
-
-    global wait_time
-    wait_time = window.after(CARD_FLIP_TIME, flip_card, "English", english_word)
+    wait_time = window.after(CARD_FLIP_TIME, flip_card)
 
 
-#   flip card to English side
-def flip_card(title, word):
+#   flip card to back side
+def flip_card():
     flash_card.itemconfig(flash_card_img, image=card_back_img)
 
-    flash_card.itemconfig(display_word, text=word, fill="white")
-    flash_card.itemconfig(display_title, text=title, fill="white")
+    flash_card.itemconfig(display_word, text=current_card['English'], fill="white")
+    flash_card.itemconfig(display_title, text="English", fill="white")
 
-    window.after_cancel(wait_time)
+
+#   Guessed card correctly
+def guessed_right():
+    words_data.remove(current_card)
+    data = pandas.DataFrame(words_data)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 # window
 window = Tk()
 window.title("Flashy")
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
+wait_time = window.after(250, next_card)
 
 # images
 card_front_img = PhotoImage(file="images/card_front.png")
@@ -55,19 +71,13 @@ flash_card_img = flash_card.create_image(400, 263, image=card_front_img)
 display_title = flash_card.create_text(400, 150, text="", font=('Ariel', 40, "italic"))
 display_word = flash_card.create_text(400, 263, text="", font=('Ariel', 60, "bold"))
 
-flash_card.itemconfig(flash_card_img, image=card_front_img)
-
 # buttons
-right_btn = Button(image=right_img, borderwidth=0, highlightthickness=0, command=next_card)
+right_btn = Button(image=right_img, borderwidth=0, highlightthickness=0, command=guessed_right)
 wrong_btn = Button(image=wrong_img, borderwidth=0, highlightthickness=0, command=next_card)
 
 # grid
 flash_card.grid(row=0, column=0, columnspan=2)
 wrong_btn.grid(row=1, column=0)
 right_btn.grid(row=1, column=1)
-
-# file
-dataframe = pandas.read_csv("data/french_words.csv")
-words_data = dataframe.to_dict(orient="records")
 
 window.mainloop()
